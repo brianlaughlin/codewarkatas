634eeebbed311a5363d07e903b2d0b33d1bde340
Brian <brian@inetoc.com>
2017-12-14 14:26:29 -0800

    Removed unnecessary variable.

--- src/main/java/kata/findthemissingletter/Kata.java
+++ src/main/java/kata/findthemissingletter/Kata.java
@@ -2,14 +2,14 @@ package kata.findthemissingletter;
 
 public class Kata {
     public static char findMissingLetter(char[] array) {
-        char missingValue = 0;
+
         char charInQueue = array[0];
         for (char anArray : array) {
             if (charInQueue != anArray) {
                 return charInQueue;
             } else charInQueue += 1;
         }
-        return missingValue;
+        return 0;
     }
 
 }
