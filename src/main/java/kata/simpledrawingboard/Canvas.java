ad5b5041e7086218346ccf3024c7c33141d5dec7
Brian <brian@inetoc.com>
2018-01-22 11:44:35 -0800

    Changed from StringBuilder to Matrix.

--- src/main/java/kata/simpledrawingboard/Canvas.java
+++ src/main/java/kata/simpledrawingboard/Canvas.java
@@ -1,19 +1,18 @@
 package kata.simpledrawingboard;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class Canvas {
     int width;
     int height;
+    int fullLength;
     StringBuilder theCanvas;
     String[][] canvasMatrix;
 
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
+        this.fullLength = width + 4; // -- + \n
         theCanvas = new StringBuilder();
-        canvasMatrix = new String[width][height];
+        canvasMatrix = new String[fullLength][height + 2];
     }
 
     public Canvas draw(int x1, int y1, int x2, int y2) {
@@ -24,21 +23,35 @@ public class Canvas {
         return this;
     }
 
-    public String drawCanvas(){
+    public String drawCanvas() {
+        drawTopAndBottom();
+        drawSides();
+        drawTopAndBottom();
 
-        return "";
+        theCanvas.setLength(0);
+        for (int row = 0; row < height+2; row++) {
+            for (int col = 0; col < fullLength; col++) {
+                if (canvasMatrix[col][row] == "\\") {
+                    theCanvas.append("\n");
+                    break;
+                } else theCanvas.append(canvasMatrix[col][row]);
+            }
+        }
+
+        return theCanvas.toString();
     }
 
 
     public String drawCanvasPassDrawLines() {
         drawBoarders();
 
+
         // 0 , 2 = 'x'
-        theCanvas.replace(11,12, "x");
-        theCanvas.replace(19,20, "x");
-        theCanvas.replace(25,30, "xxxxx");
-        theCanvas.replace(35,36, "x");
-        theCanvas.replace(43,44, "x");
+        theCanvas.replace(11, 12, "x");
+        theCanvas.replace(19, 20, "x");
+        theCanvas.replace(25, 30, "xxxxx");
+        theCanvas.replace(35, 36, "x");
+        theCanvas.replace(43, 44, "x");
 
         return theCanvas.toString();
     }
@@ -50,24 +63,48 @@ public class Canvas {
     }
 
     private void drawSides() {
-        for (int h = 0; h < height; h++) {
-            theCanvas.append("|");
-            for (int i = 0; i < width; i++) {
-                theCanvas.append(" ");
+//        for (int h = 0; h < height; h++) {
+//            theCanvas.append("|");
+//            for (int i = 0; i < width; i++) {
+//                theCanvas.append(" ");
+//            }
+//            theCanvas.append("|");
+//            theCanvas.append("\n");
+//        }
+
+        for (int h = 1; h < height + 1; h++){
+            for(int col = 0; col <= fullLength - 2; col++){
+                if(col == 0 || col == (fullLength - 3))
+                    canvasMatrix[col][h] = "|";
+                else
+                    canvasMatrix[col][h] = " ";
             }
-            theCanvas.append("|");
-            theCanvas.append("\n");
+            canvasMatrix[fullLength - 2][h] = "\\";
+            canvasMatrix[fullLength - 1][h] = "n";
         }
+
     }
 
     private void drawTopAndBottom() {
 
         Boolean isTop = true;
-        if (theCanvas.length() > 1) isTop = false;
-        theCanvas.append(IntStream.range(0, width + 2)
-                .mapToObj(i -> "-")
-                .collect(Collectors.joining()));
-        if (isTop) theCanvas.append("\n");
+        int row = 0;
+//        if (theCanvas.length() > 1) isTop = false;
+//        theCanvas.append(IntStream.range(0, width + 2)
+//                .mapToObj(i -> "-")
+//                .collect(Collectors.joining()));
+//        if (isTop) theCanvas.append("\n");
+
+        // Using String
+        if(canvasMatrix[0][0] == "-") isTop = false;
+        if(!isTop) row = height + 1;
+        for (int i = 0; i < fullLength - 2; i++) {
+            canvasMatrix[i][row] = "-";
+        }
+        if (isTop) {
+            canvasMatrix[fullLength - 2][0] = "\\";
+            canvasMatrix[fullLength - 1][0] = "n";
+        }
 
     }
 }
